8bef70076e22f1203f49f057c7cd3ad4c96854d7
s0i <harleybenoit@gmail.com>
2020-05-15 08:14:46 -0600

    Fixing issues with SuspiciousOperation error
    Trying to track pageviews when user was logged in and then logs out and session changes before processing the request.

--- privacy_analytics/middleware.py
+++ privacy_analytics/middleware.py
@@ -11,18 +11,19 @@ class AnalyticsMiddleware:
         response = self.get_response(request)
 
         if not request.META.get('HTTP_DNT'):
+            if not request.session or not request.session.session_key:
+                request.session.save()
+                
             page = {
                 'agent': request.META.get('HTTP_USER_AGENT', ''),
                 'path': request.path,
                 'referrer': request.META.get('HTTP_REFERRER', ''),
-                'session_key': request.session.session_key,
+                'session_key': request.session._get_or_create_session_key(),
                 'is_authenticated': request.user.is_authenticated
             }
 
             if hasattr(settings, 'ANALYTICS_IGNORE_PATHS'):
                 if not any(request.path.startswith(path) for path in settings.ANALYTICS_IGNORE_PATHS):
-                    if not request.session.session_key:
-                        request.session.create()
                     tasks.pageview_add_task(**page)
             else:
                 tasks.pageview_add_task(**page)
